59fa3179369dc55d9f5e16808c49c5aaa0d8d505
andz89m <andzrivero89@gmail.com>
2023-10-22 13:41:22 -0800

    fix cart

--- frontend/src/pages/users/Cart.js
+++ frontend/src/pages/users/Cart.js
@@ -78,64 +78,121 @@ const Food_list = () => {
     <>
       {deleteLoading && <LoadingSpinner />}
       <Header />
-      <div className="flex justify-center p-2 font-semibold text-2xl bg-orange-200 ">
-        Your Cart
-      </div>
-      <div className="flex justify-center   flex-wrap w-full p-5 gap-2 bg-orange-200  ">
-        {carts.map((food) => (
-          <div
-            key={food._id}
-            className="bg-white hover:bg-slate-100 w-[300px]  border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700  "
-          >
-            <img
-              className="rounded-t-lg"
-              src={food.image_one}
-              alt=""
-              style={{
-                width: "100%", // Set the image width to 100% to fit the container
-                height: "200px", // Set a fixed height for the banner
-                objectFit: "cover", // Ensure the image covers the container
-              }}
-            />
 
-            <div className="p-5">
-              <h5 className="  text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {food.food_name}
-              </h5>
-              <div className="block font-semibold text-slate-500">
-                ₱ {food.price}
-              </div>
-              <p className=" h-[50px] mb-3 font-normal text-gray-700 dark:text-gray-400  ">
-                {truncateText(food.description, maxCharacters)}
-              </p>
-              <div className="flex gap-2">
-                {" "}
-                <div className="cursor-pointer inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800">
+      <div className="flex justify-end  p-2 font-semibold   sticky top-0 z-10">
+        <div className="bg-orange-700 cursor-pointer mr-3 p-2 rounded text-white">
+          Check out
+        </div>
+      </div>
+      <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <tbody>
+            {carts.map((food) => (
+              <tr
+                key={food.food_id}
+                className="bg-white flex  flex-col justify-around sm:flex-row items-center w-full border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+              >
+                <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
                   <div className="flex items-center">
                     <input
-                      id={food._id}
+                      id={food.food_id}
                       type="checkbox"
                       value=""
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded   dark:bg-gray-700 dark:border-gray-600"
                     />
                     <label
-                      htmlFor={food._id}
-                      className="ml-2 text-sm font-medium"
+                      forhtml={food.food_id}
+                      className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                     >
                       Select
                     </label>
                   </div>
-                </div>
-                <div
-                  onClick={() => onDelete(food._id)}
-                  className="cursor-pointer inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-slate-700 rounded-lg hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300 dark:bg-slate-600 dark:hover:bg-slate-700 dark:focus:ring-slate-800"
-                >
-                  Remove <FaTrash className="ml-2" />
-                </div>
-              </div>
-            </div>
-          </div>
-        ))}
+                </td>
+                <td className="w-32 p-4">
+                  <img
+                    src={food.image_one}
+                    alt="Apple Watch"
+                    style={{
+                      width: "100px", // Set the image width to 100% to fit the container
+                      height: "100px", // Set a fixed height for the banner
+                      objectFit: "cover", // Ensure the image covers the container
+                    }}
+                  />
+                </td>
+                <td className="px-6 py-4 w-32 font-semibold text-gray-900 dark:text-white">
+                  {food.food_name}
+                </td>
+                <td className="px-6 py-4">
+                  <div className="flex items-center space-x-3">
+                    <button
+                      className="inline-flex items-center justify-center p-1 text-sm font-medium h-6 w-6 text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                      type="button"
+                    >
+                      <span className="sr-only">Quantity button</span>
+                      <svg
+                        className="w-3 h-3"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 18 2"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M1 1h16"
+                        />
+                      </svg>
+                    </button>
+                    <div>
+                      <input
+                        type="number"
+                        id="first_product"
+                        className="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="1"
+                        required
+                      />
+                    </div>
+                    <button
+                      className="inline-flex items-center justify-center h-6 w-6 p-1 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-full focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+                      type="button"
+                    >
+                      <span className="sr-only">Quantity button</span>
+                      <svg
+                        className="w-3 h-3"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 18 18"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M9 1v16M1 9h16"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </td>
+                <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                  ₱ {food.price}
+                </td>
+                <td className="px-6 py-4">
+                  <span
+                    onClick={() => onDelete(food._id)}
+                    className="font-medium text-slate-700 hover:bg-slate-700 hover:text-gray-100 cursor-pointer flex w-[100px] px-1 py-2 items-center justify-center   rounded"
+                  >
+                    Remove <FaTrash className="ml-2" />
+                  </span>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
