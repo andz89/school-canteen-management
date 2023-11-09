b11ae26e54b887f69ee3aa7e7edf6e57e2b5a27e
andz89m <andzrivero89@gmail.com>
2023-11-08 16:07:21 -0800

    fix bug in backend

--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -1,7 +1,7 @@
 import asyncHandler from "express-async-handler";
 import dotenv from "dotenv";
 dotenv.config();
-import Reviews from "../models/ReviewModel.js";
+import Reviews from "../models/reviewModel.js";
 import Order from "../models/ordersModel.js";
 
 const setNewReview = asyncHandler(async (req, res) => {
